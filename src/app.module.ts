f228db515c140fca20c86f759544ea6dbc659034
ephy-bayru <ephybayru@gmail.com>
2024-03-27 19:18:06 +0300

    chore: register LoggerService and CacheService in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { CacheService } from './common/services/cache.service';
+import { LoggingInterceptor } from './common/utils/logging.interceptor';
+import { LoggerService } from './common/services/logger.service';
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, LoggerService, CacheService, LoggingInterceptor],
+  exports: [AppService, LoggerService, CacheService, LoggingInterceptor],
 })
 export class AppModule {}
