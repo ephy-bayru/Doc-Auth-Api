da007be6d4ff81d75dd2fa5a9d18b5a50f846ce5
ephy-bayru <ephybayru@gmail.com>
2024-04-05 21:45:28 +0300

    feat(repository): implement BaseRepository with TypeORM integration
    Implemented the BaseRepository class, which conforms to the IBaseRepository interface, providing generic CRUD operations for any entity. This class utilizes TypeORM for database interactions, allowing operations such as findOne, findAll, save, update, delete, count, exists, paginate, and search on entities. It leverages TypeORM's Repository, DataSource, and various query building capabilities to offer flexible and powerful data access methods. The implementation includes detailed type annotations and method implementations to facilitate easy integration with TypeORM's features, enhancing the application's data access layer's efficiency and maintainability.

--- src/core/repository/base.repository.ts
+++ src/core/repository/base.repository.ts
@@ -8,8 +8,8 @@ import {
   SaveOptions,
   UpdateResult,
   DeleteResult,
-  InsertResult,
   FindOptionsWhere,
+  SelectQueryBuilder,
 } from 'typeorm';
 import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 
@@ -38,14 +38,24 @@ export class BaseRepository<T> implements IBaseRepository<T> {
     return this.repository.find(options);
   }
 
-  async create(entity: Partial<T>, options?: SaveOptions): Promise<T> {
-    return this.repository.save(entity as any, options);
-  }
-
-  async createMany(entities: Partial<T>[]): Promise<InsertResult> {
-    const transformedEntities: QueryDeepPartialEntity<T>[] =
-      entities as unknown as QueryDeepPartialEntity<T>[];
-    return this.repository.insert(transformedEntities);
+  async save(
+    entityOrEntities: Partial<T> | Partial<T>[],
+    options?: SaveOptions,
+  ): Promise<T | T[]> {
+    if (Array.isArray(entityOrEntities)) {
+      // Perform the bulk insert without assigning the result to a variable
+      await this.repository.insert(
+        entityOrEntities as QueryDeepPartialEntity<T>[],
+      );
+      // If the entities are returned directly without modification, ensure they match the expected type
+      // This may require ensuring the entities have all the necessary properties of `T`
+      // If you cannot guarantee the entities fully match `T` at this point, you may need to fetch them from the database
+      // after insertion or reconsider the method's return type and how it's used.
+      return entityOrEntities as T[]; // Cast as T[], acknowledging potential type safety implications
+    } else {
+      // For a single entity, return it directly after save, assuming it now fully conforms to T
+      return this.repository.save(entityOrEntities as any, options);
+    }
   }
 
   async update(
@@ -101,7 +111,117 @@ export class BaseRepository<T> implements IBaseRepository<T> {
     return { data, total, page, limit };
   }
 
-  async search(criteria: any, options: FindManyOptions<T> = {}): Promise<T[]> {
-    return await this.repository.find({ ...options, where: criteria });
+  async search(
+    criteria: any,
+    options: FindManyOptions<T> = {},
+    page: number = 1,
+    limit: number = 10,
+    sort?: { field: string; order: 'ASC' | 'DESC' },
+  ): Promise<{ data: T[]; total: number; page: number; limit: number }> {
+    const queryBuilder = this.repository.createQueryBuilder('entity');
+
+    // Handle complex criteria
+    this.applyCriteria(queryBuilder, criteria);
+
+    // Apply options to the query
+    this.applyOptions(queryBuilder, options);
+
+    // Sorting
+    if (sort) {
+      queryBuilder.orderBy(`entity.${sort.field}`, sort.order);
+    }
+
+    // Pagination
+    const [data, total] = await queryBuilder
+      .skip((page - 1) * limit)
+      .take(limit)
+      .getManyAndCount();
+
+    return { data, total, page, limit };
+  }
+
+  private applyCriteria(queryBuilder: SelectQueryBuilder<T>, criteria: any) {
+    Object.entries(criteria).forEach(([key, value]) => {
+      if (typeof value === 'object' && value !== null) {
+        Object.entries(value).forEach(([operator, opValue]) => {
+          const parameterKey = `${key}_${operator}`;
+          let queryPart;
+          switch (operator) {
+            case 'gt':
+              queryPart = `entity.${key} > :${parameterKey}`;
+              break;
+            case 'lt':
+              queryPart = `entity.${key} < :${parameterKey}`;
+              break;
+            case 'like':
+              queryPart = `entity.${key} LIKE :${parameterKey}`;
+              opValue = `%${opValue}%`;
+              break;
+            case 'in':
+              queryPart = `entity.${key} IN (:...${parameterKey})`;
+              break;
+            default:
+              queryPart = `entity.${key} = :${parameterKey}`; // Fallback to equality
+          }
+          queryBuilder.andWhere(queryPart, { [String(parameterKey)]: opValue });
+        });
+      } else {
+        // Direct equality
+        queryBuilder.andWhere(`entity.${key} = :${key}`, { [key]: value });
+      }
+    });
+  }
+
+  private applyOptions(
+    queryBuilder: SelectQueryBuilder<T>,
+    options: FindManyOptions<T>,
+  ) {
+    // Handle relations
+    if (Array.isArray(options.relations)) {
+      options.relations.forEach((relation) => {
+        queryBuilder.leftJoinAndSelect(`entity.${relation}`, relation);
+      });
+    } else if (typeof options.relations === 'object') {
+      // If relations are specified in an object format
+      Object.keys(options.relations).forEach((relationKey) => {
+        queryBuilder.leftJoinAndSelect(`entity.${relationKey}`, relationKey);
+      });
+    }
+
+    // Apply custom selections if provided
+    // Apply custom selections if provided
+    if (Array.isArray(options.select)) {
+      queryBuilder.select(
+        options.select.map((field) => `entity.${String(field)}`),
+      );
+    }
+
+    // Additional where conditions
+    if (options.where) {
+      Object.entries(options.where).forEach(([key, value]) => {
+        if (typeof value === 'object' && value !== null) {
+          // This reuses the logic from applyCriteria for complex conditions
+          Object.entries(value).forEach(([operator, opValue]) => {
+            const parameterKey = `where_${key}_${operator}`;
+            let queryPart;
+            switch (operator) {
+              // Add cases as needed, following the applyCriteria structure
+              default:
+                queryPart = `entity.${key} = :${parameterKey}`;
+            }
+            queryBuilder.andWhere(queryPart, { [parameterKey]: opValue });
+          });
+        } else {
+          queryBuilder.andWhere(`entity.${key} = :${key}`, { [key]: value });
+        }
+      });
+    }
+
+    // Ordering
+    if (options.order) {
+      Object.entries(options.order).forEach(([key, value]) => {
+        queryBuilder.addOrderBy(`entity.${key}`, value as 'ASC' | 'DESC');
+      });
+    }
   }
 }
